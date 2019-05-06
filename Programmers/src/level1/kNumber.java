59eac3123a3dce8e21e5486e878ea618a8f70b14
mukeunzi <eunji057@gmail.com>
2019-05-06 15:12:09 +0900

    Programmers k번째수 문제풀이

---
+++ Programmers/src/level1/kNumber.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Arrays;
+
+public class kNumber {
+	
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int[] copyArr = {};
+        
+        for(int i=0; i<commands.length; i++){
+            copyArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(copyArr);
+            answer[i] = copyArr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }	
+
+}

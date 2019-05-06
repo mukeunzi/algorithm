b75f1d2c7087bd6eb325d6ee7f02c13a53b58274
mukeunzi <eunji057@gmail.com>
2019-05-06 14:36:14 +0900

    BOJ 구구단 문제풀이

---
+++ BOJ/src/multiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int num = scan.nextInt();
+		
+		for(int i=1;i<10;i++) {
+			System.out.println(num + " * " + i + " = " + num*i);
+		}
+		
+		scan.close();
+	}
+}

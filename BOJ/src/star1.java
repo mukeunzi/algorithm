437378ecd9220e34a11a9fe94bc777c63255c043
mukeunzi <eunji057@gmail.com>
2019-05-06 14:33:59 +0900

    BOJ 별 찍기 - 1 문제풀이

---
+++ BOJ/src/star1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class star1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int num = scan.nextInt();
+		
+		for(int i=0;i<num;i++) {
+			for(int j=0;j<i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		scan.close();
+	}
+}

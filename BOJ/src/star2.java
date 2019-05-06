4222fc2c513d9fe7eeee9c71b9ddc2f674e3bb75
mukeunzi <eunji057@gmail.com>
2019-05-06 14:34:30 +0900

    BOJ 별 찍기 - 2 문제풀이

---
+++ BOJ/src/star2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class star2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int i, j, k;
+		int num = scan.nextInt();
+		
+		for(i=1;i<=num;i++) {
+			for(j=num-i;j>0;j--) {
+				System.out.print(" ");
+			}
+			for(k=0;k<i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		scan.close();
+	}
+}

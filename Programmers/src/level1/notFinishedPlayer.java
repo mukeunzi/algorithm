4601015a185f45f475f183ef18dc5769d377358f
mukeunzi <eunji057@gmail.com>
2019-05-06 15:12:36 +0900

    Programmers 완주하지 못한 선수 문제풀이

---
+++ Programmers/src/level1/notFinishedPlayer.java
@@ -0,0 +1,27 @@
+package level1;
+
+import java.util.Arrays;
+
+public class notFinishedPlayer {
+	
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i=0;i<completion.length;i++) {
+        	if(!participant[i].equals(completion[i])) {
+        		answer = participant[i];
+                break;
+        	}
+        }
+        
+        if(answer.equals("")) {
+        	answer = participant[participant.length-1];
+        }
+        
+        return answer;
+    }
+
+}

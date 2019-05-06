c5f0727377fbe6be913ade04b62cecaef19709bd
mukeunzi <eunji057@gmail.com>
2019-05-06 15:11:24 +0900

    Programmers 같은 숫자는 싫어 문제풀이

---
+++ Programmers/src/level1/hateSameNumber.java
@@ -0,0 +1,26 @@
+package level1;
+
+import java.util.ArrayList;
+
+public class hateSameNumber {
+	
+	public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int temp = -1;
+        
+        for(int num : arr){
+            if(temp != num){
+                list.add(num);
+            }
+            temp = num;
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+	}
+
+}

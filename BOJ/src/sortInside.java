08a50b860cc539106074bc457204ab789465ed41
mukeunzi <eunji057@gmail.com>
2019-05-06 14:33:09 +0900

    BOJ 소트인사이드 문제풀이

---
+++ BOJ/src/sortInside.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class sortInside {
+
+	public static void main(String[] args) {
+		String strNum = "";
+		int intNum [];
+		
+		Scanner scan = new Scanner(System.in);
+		strNum = scan.nextLine();
+		
+		intNum = new int[strNum.length()];
+		for(int i=0; i<strNum.length(); i++) {
+			intNum[i] = strNum.charAt(i) - '0';
+		}
+		
+		Arrays.sort(intNum);
+		
+		for(int i=intNum.length-1; i>=0; i--) {
+			System.out.print(intNum[i]);
+		}
+		
+		scan.close();
+	}
+
+}

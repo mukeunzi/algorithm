19bdfe565e8f3263176fcf1b9b4e1dd06b904fae
mukeunzi <eunji057@gmail.com>
2019-05-08 15:32:59 +0900

    Programmers 문자열 내 p와 y의 개수 문제풀이

---
+++ Programmers/src/level1/pyInString.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class pyInString {
+
+	boolean solution(String s) {
+		int pCount = 0;
+		int yCount = 0;
+		
+		for(int i=0; i<s.length(); i++){
+			if(s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+					pCount++;
+			}
+			if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+				yCount++;
+			}
+		}
+		
+		if(pCount == yCount){
+			return true;
+		}
+		return false;
+	}
+	
+}

3387b15695336476647bb56f9f050124dd7c4678
mukeunzi <eunji057@gmail.com>
2019-05-09 15:41:07 +0900

    Programmers 시저 암호 문제풀이

---
+++ Programmers/src/level1/caesarCipher.java
@@ -0,0 +1,27 @@
+package level1;
+
+public class caesarCipher {
+	
+	public String solution(String s, int n) {
+		String answer = "";
+		
+		for(int i=0; i<s.length(); i++){
+			if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+				if((int)(s.charAt(i)) + n > 122){
+					answer += (char)(((int)s.charAt(i)) + n - 26);
+				}else{
+					answer += (char)(((int)s.charAt(i)) + n);
+				}
+			}else if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+				if(((int)s.charAt(i)) + n > 90){
+					answer += (char)(((int)s.charAt(i)) + n - 26);
+				}else{
+					answer += (char)(((int)s.charAt(i)) + n);
+				}
+			}else{
+				answer += " ";
+			}
+		}
+		return answer;
+	}
+}

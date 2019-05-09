3191800ae9db3b84f529cad1aebb11452dc1477d
mukeunzi <eunji057@gmail.com>
2019-05-09 11:43:25 +0900

    Programmers 하샤드 수 문제풀이

---
+++ Programmers/src/level1/harshadNumber.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class harshadNumber {
+	
+	public boolean solution(int x) {
+		String strX = Integer.toString(x);
+		int sum = 0;
+		
+		for(int i=0; i<strX.length(); i++){
+			sum += strX.charAt(i) - '0';
+		}
+		return (x % sum == 0);
+	}
+
+}

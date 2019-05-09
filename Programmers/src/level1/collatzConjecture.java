4a067f60d0482f2a7ef5cf2dfbcc7361cfc970a4
mukeunzi <eunji057@gmail.com>
2019-05-09 11:47:12 +0900

    Programmers 콜라츠 추측 문제풀이
    - for문을 활용한 방식

---
+++ Programmers/src/level1/collatzConjecture.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class collatzConjecture {
+	
+	public int solution(int num) {      
+		for(int i=0; i<500; i++){
+			if(num == 1){
+				return i;
+			}
+			if(num % 2 == 0){
+				num = num / 2;
+			}else if(num % 2 == 1){
+				num = num * 3 + 1;
+			}
+		}
+		return -1;
+	}
+
+}

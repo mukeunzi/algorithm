021f1364cf62d6d6eba6c514abd5b7ed3705dccb
mukeunzi <eunji057@gmail.com>
2019-05-10 11:12:46 +0900

    Programmers 정수 제곱근 판별

---
+++ Programmers/src/level1/checkSqrt.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class checkSqrt {
+
+	public long solution(long n) {
+		if(Math.pow(Math.ceil(Math.sqrt(n)), 2) == n){
+			return (long)Math.pow((int)Math.sqrt(n) + 1, 2);
+		}
+		return -1;
+	}
+
+}

27caefed51ef85e46238faab14f93c0c77a4a97c
mukeunzi <eunji057@gmail.com>
2019-05-21 10:44:31 +0900

    Programmers 완주하지 못한 선수 문제풀이(2)
    - 이전 코드와 다른 점 : 불필요한 변수 제거

--- Programmers/src/level1/NotFinishedPlayer.java
+++ Programmers/src/level1/NotFinishedPlayer.java
@@ -5,23 +5,16 @@ import java.util.Arrays;
 public class NotFinishedPlayer {
 	
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
         Arrays.sort(participant);
         Arrays.sort(completion);
         
         for(int i=0;i<completion.length;i++) {
         	if(!participant[i].equals(completion[i])) {
-        		answer = participant[i];
-                break;
+        		return participant[i];
         	}
         }
         
-        if(answer.equals("")) {
-        	answer = participant[participant.length-1];
-        }
-        
-        return answer;
+        return participant[participant.length-1];
     }
 
 }

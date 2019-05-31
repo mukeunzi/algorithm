eff5d8a6add6ddf2a50f09a768ef23d41864d33c
mukeunzi <eunji057@gmail.com>
2019-05-31 11:52:33 +0900

    Programmers 전화번호 목록 문제풀이(2)

--- Programmers/src/level2/PhoneBook.java
+++ Programmers/src/level2/PhoneBook.java
@@ -1,21 +1,13 @@
 class PhoneBook {
-    public boolean solution(String[] phone_book) {
-        String temp = "";
-        
+    public boolean solution(String[] phone_book) {        
         for(int i=0; i<phone_book.length-1; i++){
-            for(int j=1; j<phone_book.length; j++){
-                if(phone_book[i].length() > phone_book[j].length()){
-                    temp = phone_book[i].substring(0, phone_book[j].length());
-                    if(temp.equals(phone_book[j])){
-                        return false;
-                    }
-                }else if(phone_book[i].length() < phone_book[j].length()){
-                    temp = phone_book[j].substring(0, phone_book[i].length());
-                    if(temp.equals(phone_book[i])){
-                        return false;
-                    }
+            for(int j=i+1; j<phone_book.length; j++){
+                if(phone_book[i].startsWith(phone_book[j])){
+                    return false;
                 }
-                temp = "";
+                if(phone_book[j].startsWith(phone_book[i])){
+                    return false;
+                }
             }
         }
         return true;

bb2d39dcd166906500803940c7a8e5f9dc07c5cc
mukeunzi <eunji057@gmail.com>
2019-06-02 17:00:02 +0900

    Programmers 최솟값 만들기 문제풀이

---
+++ Programmers/src/level2/MakeMinNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class MakeMinNumber{
+    public int solution(int []A, int []B){
+        int sum = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0; i<A.length; i++){
+            sum += A[i] * B[A.length-1-i];
+        }
+        
+        return sum;
+    }
+}
